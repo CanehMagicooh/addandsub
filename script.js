cf53150754d5d9b5298bf1c880b6c70c007734bb
CanehMagicooh <46936829+CanehMagicooh@users.noreply.github.com>
2020-03-21 16:43:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function Calculate() {
+    var Prova = 11
     var Number1 = parseInt(document.getElementById("Number1").value)
     var Number2 = parseInt(document.getElementById("Number2").value)
+    var Oper = document.getElementById("Operator").value
     var Plus = Number1 + Number2
+    if (Prova > 10) {
     document.getElementById("Result").innerHTML = Plus
+    } else {
+   document.getElementById("Result").innerHTML = "Sas"
+    }
 }
