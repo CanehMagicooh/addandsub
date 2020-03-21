2d9868c77bc8ce108a2c912d88692dd078ea8681
CanehMagicooh <46936829+CanehMagicooh@users.noreply.github.com>
2020-03-21 13:37:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
-function Result() {
-	var a = parseInt(document.getElementById('FirstNumber').value);
-	var b = parseInt(document.getElementById('SecondNumber').value);
-	var c = parseInt(document.getElementById('ThreeNumber').value);
-	var numeroR = b * b - a * 4 * c
-    var Radice = Math.pow(numeroR, 0.5);
-    var Risultato001 = -b + Radice
-    var Risultato01 = 2 * a
-    var Risultato1 = Risultato001 / Risultato01
-    var Risultato002 = -b - Radice
-    var Risultato02 = 2 * a
-    var Risultato2 = Risultato002 / Risultato02
+var Number1 = parseInt(document.getElementById("Number1").innerHTML)
+var Number2 = parseInt(document.getElementById("Number2").innerHTML)
+var Operator = document.getElementById("Operator").innerHTML
 
-	document.getElementById("Result1").innerHTML = "X1: " + Risultato1
-	document.getElementById("Result2").innerHTML = "X2: " + Risultato2
+function Calculate() {
+	var Number1 = parseInt(document.getElementById("Number1").innerHTML)
+    var Number2 = parseInt(document.getElementById("Number2").innerHTML)
+    var Operator = document.getElementById("Operator").innerHTML
+    
+	if(Operator = "+") {
+		var Plus = Number1 + Number2
+		document.getElementById("Result").innerHTML = "= " + Plus
+    }
+    if(Operator = "-") {
+    	var Minor = Number1 - Number2
+		document.getElementById("Result").innerHTML = "= " + Minor
+    }
+     if(Operator = "x") {
+     	var Multiple = Number1 * Number2
+		document.getElementById("Result").innerHTML = "= " + Multiple
+    }
+     if(Operator = ":") {
+     	var Division = Number1 / Number2
+		document.getElementById("Result").innerHTML = "= " + Division
+    }
 }
