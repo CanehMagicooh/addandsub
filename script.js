f4c104442847e4f3cc49bb360daf4e274ab766a5
CanehMagicooh <46936829+CanehMagicooh@users.noreply.github.com>
2020-03-21 16:52:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,23 @@ function Calculate() {
     var Number2 = parseInt(document.getElementById("Number2").value)
     var Oper = document.getElementById("Operator").value
     var Add = "+"
+    var Sott = "-"
+    var Mult = "x"
+    var Div = ":"
     var Plus = Number1 + Number2
+    var Minor = Number1 - Number2
+    var Multiple = Number1 * Number2
+    var Division = Number1 / Number2
     if (Oper === Add) {
     document.getElementById("Result").innerHTML = Plus
-    } else {
-   document.getElementById("Result").innerHTML = "Sas"
+    }
+    if (Oper === Sott) {
+    	document.getElementById("Result").innerHTML = Minor
+    }
+    if (Oper === Mult) {
+    	document.getElementById("Result").innerHTML = Multiple
+    }
+    if (Oper === Div) {
+    	document.getElementById("Result").innerHTML = Division
     }
 }
