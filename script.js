fddf023a807450f04831edd891b21155a3581884
CanehMagicooh <46936829+CanehMagicooh@users.noreply.github.com>
2020-03-21 13:42:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-var Number1 = parseInt(document.getElementById("Number1").innerHTML)
-var Number2 = parseInt(document.getElementById("Number2").innerHTML)
-var Operator = document.getElementById("Operator").innerHTML
-
 function Calculate() {
 	var Number1 = parseInt(document.getElementById("Number1").innerHTML)
     var Number2 = parseInt(document.getElementById("Number2").innerHTML)
