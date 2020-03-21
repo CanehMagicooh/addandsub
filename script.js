b62e5b6b660f28a0b235e22b09fab9fc31473ec9
CanehMagicooh <46936829+CanehMagicooh@users.noreply.github.com>
2020-03-21 14:13:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ function Calculate() {
     var Number1 = parseInt(document.getElementById("Number1").value);
     var Number2 = parseInt(document.getElementById("Number2").value);
     var Operator = document.getElementById("Operator").innerHTML
+    var Plus = "+"
 
-     if(Operator === "+") {
+     if(Operator = Plus) {
 	  document.getElementById("Result").innerHTML = Number1 + Number2
     }
 }
