7447228a9f199b8afcdc4ccc7d77e05b8d08f50b
CanehMagicooh <46936829+CanehMagicooh@users.noreply.github.com>
2020-03-21 14:02:17 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
+
 function Calculate() {
 	var Number1 = parseInt(document.getElementById("Number1").innerHTML)
     var Number2 = parseInt(document.getElementById("Number2").innerHTML)
     var Operator = document.getElementById("Operator").innerHTML
+    var Plus = Number1 + Number2
+    var Minor = Number1 - Number2
+    var Multiple = Number1 * Number2
+    var Multiple = Number1 * Number2
 
 	if(Operator = "+") {
-		var Plus = Number1 + Number2
 		document.getElementById("Result").innerHTML = "= " + Plus
     }
     if(Operator = "-") {
-    	var Minor = Number1 - Number2
 		document.getElementById("Result").innerHTML = "= " + Minor
     }
      if(Operator = "x") {
-     	var Multiple = Number1 * Number2
 		document.getElementById("Result").innerHTML = "= " + Multiple
     }
      if(Operator = ":") {
-     	var Division = Number1 / Number2
 		document.getElementById("Result").innerHTML = "= " + Division
     }
 }
