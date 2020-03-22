90b3648126671f5e6aeefdfb0f0f19227cbf6549
CanehMagicooh <46936829+CanehMagicooh@users.noreply.github.com>
2020-03-22 15:08:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,23 @@
-function Calculate() {
-    var Number1 = parseInt(document.getElementById("Number1").value)
-    var Number2 = parseInt(document.getElementById("Number2").value)
-    var Oper = document.getElementById("Operator").value
-    var Add = "+"
-    var Sott = "-"
-    var Mult = "x"
-    var Div = ":"
-    var Plus = Number1 + Number2
-    var Minor = Number1 - Number2
-    var Multiple = Number1 * Number2
-    var Division = Number1 / Number2
-    if (Oper === Add) {
-    document.getElementById("Result").innerHTML = Number1 + " + " + Number2 + " = " + Plus
-    }
-    if (Oper === Sott) {
-    	document.getElementById("Result").innerHTML = Number1 + " - " + Number2 +  " = " + Minor
-    }
-    if (Oper === Mult) {
-    	document.getElementById("Result").innerHTML = Number1 + " x " + Number2 +  " = " + Multiple
-    }
-    if (Oper === Div) {
-    	document.getElementById("Result").innerHTML = Number1 + " : " + Number2 +  " = " + Division
-    }
+var Result = 0
+
+function Plus() {
+  var ResultInput = parseInt(document.getElementById('UserInput').value);
+  Result = Result + ResultInput
+    if (Result <= 99999999) {
+  document.getElementById('Result').innerHTML = Result
+} else {
+  Result = 0
+  document.getElementById('Result').innerHTML = Result
+}
 }
 
-
-function Result() {
-	var a = parseInt(document.getElementById('FirstNumber').value);
-	var b = parseInt(document.getElementById('SecondNumber').value);
-	var c = parseInt(document.getElementById('ThreeNumber').value);
-	var numeroR = b * b - a * 4 * c
-    var Radice = Math.pow(numeroR, 0.5);
-    var Risultato001 = -b + Radice
-    var Risultato01 = 2 * a
-    var Risultato1 = Risultato001 / Risultato01
-    var Risultato002 = -b - Radice
-    var Risultato02 = 2 * a
-    var Risultato2 = Risultato002 / Risultato02
-
-	document.getElementById("Result1").innerHTML = "X1: " + Risultato1
-	document.getElementById("Result2").innerHTML = "X2: " + Risultato2
+function Minor() {
+  var ResultInput = parseInt(document.getElementById('UserInput').value);
+  Result = Result - ResultInput
+  if (Result >= 1) {
+document.getElementById('Result').innerHTML = Result
+} else {
+Result = 0
+document.getElementById('Result').innerHTML = Result
+}
 }
